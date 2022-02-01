b0019311e4d621b7bf4eedec885b4557e42db75f
mazen Aldebeat <mazend76@gmail.com>
2022-02-02 00:00:37 +0100

    Header_

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="layout">
+        <Header/>
+        <main>
+            main
+        </main>
+        <footer>
+            footer
+        </footer>
     </div>
   );
 }
