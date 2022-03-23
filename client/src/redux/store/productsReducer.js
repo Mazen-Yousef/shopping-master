8fdff3f4da497943422927bb5622fc147b3b53e6
mazen Aldebeat <mazend76@gmail.com>
2022-03-24 00:08:29 +0100

    redux filter

--- client/src/redux/store/productsReducer.js
+++ client/src/redux/store/productsReducer.js
@@ -1,10 +1,26 @@
-import { FETCH_PRODUCTS } from "../actions/types";
+import { FETCH_PRODUCTS, FILTER_SIZE, FILTER_SORT } from "../actions/types";
 
 export const productsReducer = (state = {}, action)=>{
     switch (action.type){
         case FETCH_PRODUCTS :
-            return {products : action.data}
-            default : return state
+            return {
+                    products : action.data,
+                    productsByFilter : action.data
+                   }
+        case FILTER_SIZE :
+            return {
+                    ...state,
+                    productsByFilter : action.data.products,
+                    size : action.data.size
+                   }
+        case FILTER_SORT : 
+            return {
+                    ...state,
+                    productsByFilter : action.data.products,
+                    sort : action.data.sort
+                   }  
+
+        default : return state
     }
 
 }
